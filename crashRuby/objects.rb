ef08736cddf2ea5cad90ebc805a49765c7b09a10
Uzo AtHome <segmond@gmail.com>
2015-07-17 00:10:00 -0400

    objects

---
+++ crashRuby/objects.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+s1 = String.new "Ruby"
+puts s1.size
+puts s1.downcase
+
+a1 = Array.new
+a1.push 1,2,3
+puts a1.include? 3
+puts a1.empty?
+
+r1 = Range.new 1,6
+puts r1.class
+puts r1.include? 4
