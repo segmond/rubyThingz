20efa198083a7bba48a129cbf9ffccb6f8a7c05a
Uzo AtHome <segmond@gmail.com>
2015-08-05 01:10:41 -0400

    updatE

--- crashRuby/regex.rb
+++ crashRuby/regex.rb
@@ -22,3 +22,40 @@ p $&    # matched text
 p $'    # text after last match
 
 # /\bword\b/ set word boundaries, match whole word instead of word within a string
+
+pattern = /[fs]it/
+words = %w/ sit MIT fit fat lot pad/
+
+words.each do |word|
+    if word.match pattern
+        puts "#{word} matches the pattern"
+    else
+        puts "#{word} doesn't matches the pattern"
+    end
+end
+
+# quantifiers
+#
+# ? - 0 or 1 match
+# * - 0 or more
+# + - 1 or more
+# {n} exactly n
+# {n,} n or more
+# {,n} n or less
+# {n,m} range n to m
+
+
+emails = %w/ luke@gmail.com andy@yahoo.com 23214sdj^as
+    f3444@gmail.com /
+    
+pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/
+
+emails.each do |email| 
+
+    if email.match pattern
+        puts "#{email} matches"
+    else
+        puts "#{email} does not match"
+    end
+    
+end
